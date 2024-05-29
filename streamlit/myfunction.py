742b31f546420d0b7f69f2a2f8ec0a4c680b2d7d
crab409 <152848510+crab409@users.noreply.github.com>
2024-05-29 14:17:29 +0900

    Add files via upload

--- streamlit/myfunction.py
+++ streamlit/myfunction.py
@@ -2,8 +2,9 @@ def is_user(id_in, password) :
     f = open("user_info.txt", "r") 
     lines = f.readlines()
     for line in lines : 
-        id_info = (line.split(','))[0]
-        password_info = (line.split(','))[1]
+        user_info = line.strip().split(',')
+        id_info = user_info[0]
+        password_info = user_info[1]
         if (id_in==id_info) :
             if (password==password_info) :
                 return True 
---
+++ streamlit/pages/report.py
@@ -0,0 +1,3 @@
+import streamlit 
+
+streamlit.title("정답 제출하기")
